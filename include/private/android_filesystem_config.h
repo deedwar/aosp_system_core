b2739945e64eec68e808bccaeefbd17ddd6fe872
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-07-17 12:04:02 +0100

    Explicitly add the theme manager's AID to fs config
    SELinux failed to set the theme manager's context since the
    app has a fixed non-user ID, and it didn't map to a pseudo-username.
    
    Change-Id: I287dc0dd02abc164fe22e14c9f3f505c3dcb938d
    
    Conflicts:
    	include/private/android_filesystem_config.h

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -77,6 +77,8 @@
 #define AID_SDCARD_AV     1034  /* external storage audio/video access */
 #define AID_SDCARD_ALL    1035  /* access all users external storage */
 
+#define AID_THEMEMAN      1300  /* theme manager */
+
 #define AID_SHELL         2000  /* adb and debug shell user */
 #define AID_CACHE         2001  /* cache access */
 #define AID_DIAG          2002  /* access to diagnostic resources */
@@ -167,6 +169,7 @@ static const struct android_id_info android_ids[] = {
 
     { "misc",          AID_MISC, },
     { "nobody",        AID_NOBODY, },
+    { "theme_man", AID_THEMEMAN },
 };
 
 #define android_id_count \
