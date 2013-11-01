7066fd085774d102abfbce1246febaf14a56426d
Bhakthavatsala Raghavendra <braghave@codeaurora.org>
2013-11-01 15:20:46 -0700

    Bluetooth: Enable system processes to set bluetooth properties
    ANT and BT is part of the same wcnsss subsystem and hence libbt-vendor
    is being used by both ANT and BT stack for hardware initialization.
    ANT stack runs in system permission which loads libbt-vendor which in turn
    needs write access to bluetooth. properties.
    
    Change-Id: I29a9b74504f1c03b98db3cddb2a53399f4b6115b
    
    Conflicts:
    
    	init/property_service.c

--- init/property_service.c
+++ init/property_service.c
@@ -85,7 +85,7 @@ struct {
     { "service.",         AID_SYSTEM,   0 },
     { "wlan.",            AID_SYSTEM,   0 },
     { "gps.",             AID_GPS,      0 },
-    { "bluetooth.",       AID_BLUETOOTH,    0 },
+    { "bluetooth.",       AID_BLUETOOTH,   AID_SYSTEM },
     { "dhcp.",            AID_SYSTEM,   0 },
     { "dhcp.",            AID_DHCP,     0 },
     { "debug.",           AID_SYSTEM,   0 },
